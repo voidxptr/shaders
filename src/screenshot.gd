1c61416e1568f60b48d49e4002acbcb5aeea2738
0xab7a3ffab8 <0xab7a3ffab8@gmail.com>
2023-08-26 12:31:29 +0200

    added screenshot script and shortcut

---
+++ src/screenshot.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const SCREEN_SHOT_PATH:String = "res://renders/"
+
+func takeScreenshot()->Image:
+	return get_tree().get_root().get_texture().get_image()
+
+func render()->void:
+	var image:Image = takeScreenshot()
+	image.flip_y()
+	
+	
+	image.save_png(SCREEN_SHOT_PATH + "/" + str(Time.get_unix_time_from_system()) + ".png" )
+	
+
+func _physics_process(_delta)->void:
+	if Input.is_action_just_released("render"):
+		render()
